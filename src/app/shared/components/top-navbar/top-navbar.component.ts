da601edc899da175f101c9560c94880be543c490
mimidots <murage@labskenya.com>
2021-01-14 21:58:26 +0300

    Update angular to v11

--- src/app/shared/components/top-navbar/top-navbar.component.ts
+++ src/app/shared/components/top-navbar/top-navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {CartService} from '../../../singleton/services/cart/cart.service';
 
 @Component({
@@ -6,16 +6,15 @@ import {CartService} from '../../../singleton/services/cart/cart.service';
     templateUrl: './top-navbar.component.html',
     styleUrls: ['./top-navbar.component.sass']
 })
-export class TopNavbarComponent implements OnInit {
+export class TopNavbarComponent {
 
     @Input() hasSearchMenu = true;
     @Input() hasCartMenu = true;
     @Input() hasAccountMenu = true;
+    @Input() totalCartItems = 0;
 
     constructor(private cartService: CartService) {
     }
 
-    ngOnInit() {
-    }
 
 }
